94f4a6addfdb73031d00e45971f7000aad450a0b
Lindsey Levie <hcrt@gmail.com>
2026-08-07 14:00:16 -0400

    Prepared for initial student use.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BenchLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BenchLinearOpMode.java
@@ -6,33 +6,21 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
-
 
 /*
  * This file contains a minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
  * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
  * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
  * class is instantiated on the Robot Controller and executed.
- *
- * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
- * It includes all the skeletal structure that all linear OpModes contain.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Bench Linear OpMode", group="Linear OpMode")
+@TeleOp(name="Bench 2 Linear OpMode", group="Linear OpMode")
 //@Disabled
 public class BenchLinearOpMode extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor benchMotor;
-    //private DigitalChannel digitalTouch;
-    //private DistanceSensor sensorV3;
-    //private Servo serve2arm;
-    //private Servo servo6arm;
 
     @Override
     public void runOpMode() {
@@ -48,13 +36,13 @@ public class BenchLinearOpMode extends LinearOpMode {
 
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
-            tgtPower = this.gamepad1.left_stick_y;
+            tgtPower = gamepad1.left_stick_y;
             benchMotor.setPower(tgtPower);
 
             // Show the elapsed game time.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
-            telemetry.addData("target benchMotor power: ", tgtPower);
-            telemetry.addData("benchMotor power: ", benchMotor.getPower());
+            telemetry.addData("benchMotor target power: ", tgtPower);
+            telemetry.addData("benchMotor actual power: ", benchMotor.getPower());
             telemetry.update();
         }
     }
