a59861eab14367d982f2c567fa439889299d9915
Lindsey Levie <hcrt@gmail.com>
2026-08-17 17:24:44 -0400

    Started with tank drive for Jarvis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JarvisLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JarvisLinearOpMode.java
@@ -1,12 +1,10 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.Range;
 
 
 /*
@@ -24,7 +22,7 @@ import com.qualcomm.robotcore.util.Range;
 
 @TeleOp(name="Jarvis Drive Linear OpMode", group="Linear OpMode")
 //@Disabled
-public class BenchLinearOpMode extends LinearOpMode {
+public class JarvisLinearOpMode extends LinearOpMode {
 
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
