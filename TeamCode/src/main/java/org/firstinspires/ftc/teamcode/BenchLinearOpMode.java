11c39f07db051544ff24b0f78a85c2083d8cf08b
FlyingLegion <hcrt3635@gmail.com>
2026-08-17 17:21:47 -0400

    student updates to bench1 as of 17Aug26

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BenchLinearOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BenchLinearOpMode.java
@@ -1,11 +1,16 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.rev.RevColorSensorV3;
+import com.qualcomm.hardware.rev.RevTouchSensor;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 /*
  * This file contains a minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
@@ -21,11 +26,21 @@ public class BenchLinearOpMode extends LinearOpMode {
     // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor benchMotor;
+    private Servo servo2arm;
+    private Servo servo6arm;
+    private RevColorSensorV3 sensorV3;
+    private RevTouchSensor button;
+
 
     @Override
     public void runOpMode() {
         benchMotor = hardwareMap.get(DcMotor.class, "motor");
+        servo2arm = hardwareMap.get(Servo.class, "servo2arm");
+        servo6arm = hardwareMap.get(Servo.class, "servo6arm");
+        sensorV3 = hardwareMap.get(RevColorSensorV3.class, "sensorColorV3");
+        button = hardwareMap.get(RevTouchSensor.class,"button");
         double tgtPower = 0;
+        double jeff = 0;
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -39,10 +54,35 @@ public class BenchLinearOpMode extends LinearOpMode {
             tgtPower = gamepad1.left_stick_y;
             benchMotor.setPower(tgtPower);
 
+            if(gamepad1.left_trigger_pressed) {
+                servo2arm.setPosition(0.5);
+            }else if(gamepad1.right_trigger_pressed) {
+                servo2arm.setPosition(1);
+            }
+         // run until not pressed
+            if (gamepad1.right_bumper ) {
+                servo6arm.setPosition(1);
+            }
+            else if(gamepad1.left_bumper ) {
+                servo6arm.setPosition(0);}
+            else {
+                servo6arm.setPosition(0.5);
+            }
+
+            sensorV3.getDistance(DistanceUnit.CM);
+
+            //if(button.isPressed()){
+            //
+            //}
+
             // Show the elapsed game time.
             telemetry.addData("Status", "Run Time: " + runtime.toString());
             telemetry.addData("benchMotor target power: ", tgtPower);
             telemetry.addData("benchMotor actual power: ", benchMotor.getPower());
+            telemetry.addData("servo2arm position: ", servo2arm.getPosition());
+            telemetry.addData("servo6arm speed: ", servo6arm.getPosition());
+            telemetry.addData("sensorV3 object distance: ", sensorV3.getDistance (DistanceUnit.CM));
+            telemetry.addData("button touched: ", button.getValue());
             telemetry.update();
         }
     }
